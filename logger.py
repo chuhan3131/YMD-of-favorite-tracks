6d6d0d77a56998bcf5e54c39cfdcc8c8d59125f9
chuhan <chuhanchik3131@gmail.com>
2025-11-22 16:59:39 +0500

    style: added gray color

--- logger.py
+++ logger.py
@@ -1,45 +1,40 @@
 import logging
+import sys
+from colorama import init, Fore, Back, Style
+
+init()
 
 COLORS = {
-    'DEBUG': '\033[36m',      
-    'INFO': '\033[32m',      
-    'WARNING': '\033[33m',   
-    'ERROR': '\033[31m',     
-    'CRITICAL': '\033[35m',   
-    'RESET': '\033[0m',      
-    'BLUE': '\033[34m',      
-    'MAGENTA': '\033[35m',    
-    'CYAN': '\033[36m',      
-    'WHITE': '\033[37m',     
-    'YELLOW': '\033[93m',   
-    'RED': '\033[91m',      
-    'GREEN': '\033[92m',     
+    'DEBUG': Fore.CYAN,
+    'INFO': Fore.WHITE, 
+    'WARNING': Fore.YELLOW,
+    'ERROR': Fore.RED,
+    'CRITICAL': Fore.RED + Style.BRIGHT,
+    'RESET': Style.RESET_ALL,
+    'BLUE': Fore.BLUE,
+    'MAGENTA': Fore.MAGENTA,
+    'CYAN': Fore.CYAN,
+    'WHITE': Fore.WHITE,
+    'YELLOW': Fore.YELLOW,
+    'RED': Fore.RED,
+    'GREEN': Fore.GREEN,
+    'GRAY': Fore.WHITE
 }
 
-def color_formatter(record):
-    original_msg = record.msg
-    
-    color = COLORS.get(record.levelname, COLORS['INFO'])
-    
-    if color:
-        record.msg = f"{color}{original_msg}{COLORS['RESET']}"
-    
-    formatter = logging.Formatter(
-        '%(asctime)s - %(message)s',
-        datefmt='%H:%M:%S'
-    )
-    return formatter.format(record)
-
 def setup_logger():
-    logger = logging.getLogger(__name__)
+    logger = logging.getLogger('YMDownloader')
     logger.setLevel(logging.INFO)
     
-    handler = logging.StreamHandler()
+    if logger.handlers:
+        return logger
+    
+    handler = logging.StreamHandler(sys.stdout)
     handler.setLevel(logging.INFO)
     
     handler.setFormatter(logging.Formatter('%(asctime)s - %(message)s', datefmt='%H:%M:%S'))
     
     logger.addHandler(handler)
+    logger.propagate = False
     
     return logger
 
@@ -55,8 +50,8 @@ def error(text):
 def info(text):
     return f"{COLORS['BLUE']}{text}{COLORS['RESET']}"
 
-def track(text):
-    return f"{COLORS['MAGENTA']}{text}{COLORS['RESET']}"
-
 def debug(text):
     return f"{COLORS['CYAN']}{text}{COLORS['RESET']}"
+
+def gray(text):
+    return f"{COLORS['GRAY']}{text}{COLORS['RESET']}"
